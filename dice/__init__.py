4d4c52a6137cada55a7b7fe7cfcd95522143ad9a
Sam Clements <sam@borntyping.co.uk>
2013-08-25 13:29:49 +0100

    Added the --verbose command line option

--- dice/__init__.py
+++ dice/__init__.py
@@ -7,38 +7,39 @@ import argparse
 import dice.grammar
 import dice.utilities
 
-__all__ = ['parse', 'roll', 'main', 'ParseException']
+__all__ = ['roll', 'main', 'ParseException']
 __author__ = "Sam Clements <sam@borntyping.co.uk>"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 from pyparsing import ParseException
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    '-v', '--version', action='version',
+    '-V', '--version', action='version',
     version='dice v{0} by {1}'.format(__version__, __author__))
+parser.add_argument(
+    '-v', '--verbose', action='store_true',
+    help="show additional output")
 parser.add_argument(
     'expression',
     help="the expression to parse and roll")
 
-def _(obj):
-    print(obj)
-    return obj
-
 def parse(string, grammar):
     """Returns an AST parsed from an expression"""
-    return _(grammar.parseString(string, parseAll=True))
+    return grammar.parseString(string, parseAll=True)
 
-def evaluate(string, grammar):
+def evaluate(string, grammar, **options):
     """Parse and then evaluate a string with a grammar"""
-    return _([element.evaluate() for element in parse(string, grammar)])
+    return [e.evaluate(**options) for e in parse(string, grammar)]
 
-def roll(string):
+def roll(string, verbose=False):
     """Parses and evaluates an expression"""
-    return evaluate(string, dice.grammar.expression)
+    return evaluate(string, grammar=dice.grammar.expression, verbose=verbose)
 
 def main(argv=None):
     args = parser.parse_args(argv)
-    result = roll(args.expression)
-    print("Result:", dice.utilities.single(result))
+    result = roll(args.expression, verbose=args.verbose)
+    result = dice.utilities.single(result)
+    if args.verbose:
+        print("Result:", end=" ")
     return result
