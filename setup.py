f19841c960944604a4e95f978ae5ee451832f861
Sam Clements <sam@borntyping.co.uk>
2016-07-12 14:44:39 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name             = 'dice',
-    version          = '1.0.3',
+    version          = '1.1.0',
 
     author           = "Sam Clements",
     author_email     = "sam@borntyping.co.uk",
