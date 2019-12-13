b52e3b30265db1d8f1b66f01ff646a6bab302a09
Sam Clements <sam@borntyping.co.uk>
2019-12-13 09:07:33 +0000

    Bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dice",
-    version="3.0.0",
+    version="3.1.0",
     author="Sam Clements",
     author_email="sam@borntyping.co.uk",
     url="https://github.com/borntyping/python-dice",
