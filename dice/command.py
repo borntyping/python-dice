20183f1be2403b2b58e03a1287b0651e5f48afd3
Sam Clements <sam@borntyping.co.uk>
2023-05-18 18:15:25 +0100

    Reformat code

--- dice/command.py
+++ dice/command.py
@@ -18,13 +18,44 @@ import dice.exceptions
 
 __version__ = "dice v{0} by {1}.".format(dice.__version__, dice.__author__)
 
-parser = argparse.ArgumentParser(prog="dice", description="Parse and evaluate dice notation.", epilog=__version__)
-parser.add_argument('-m', '--min', action="store_true", help="Make all rolls the lowest possible result.")
-parser.add_argument('-M', '--max', action="store_true", help="Make all rolls the highest possible result.")
-parser.add_argument('-D', '--max-dice', action="store", type=int, metavar='N', help="Set the maximum number of dice per element.")
-parser.add_argument('-v', '--verbose', action="store_true", help="Show additional output.")
-parser.add_argument('-V', '--version', action="version", version=__version__, help="Show the package version.")
-parser.add_argument('expression', nargs='+')
+parser = argparse.ArgumentParser(
+    prog="dice", description="Parse and evaluate dice notation.", epilog=__version__
+)
+parser.add_argument(
+    "-m",
+    "--min",
+    action="store_true",
+    help="Make all rolls the lowest possible result.",
+)
+parser.add_argument(
+    "-M",
+    "--max",
+    action="store_true",
+    help="Make all rolls the highest possible result.",
+)
+parser.add_argument(
+    "-D",
+    "--max-dice",
+    action="store",
+    type=int,
+    metavar="N",
+    help="Set the maximum number of dice per element.",
+)
+parser.add_argument(
+    "-v", "--verbose", action="store_true", help="Show additional output."
+)
+parser.add_argument(
+    "-V",
+    "--version",
+    action="version",
+    version=__version__,
+    help="Show the package version.",
+)
+parser.add_argument(
+    "expression",
+    nargs="+",
+    help="One or more expressions in dice notation",
+)
 
 
 def main(args=None):
@@ -40,7 +71,7 @@ def main(args=None):
         f_roll = dice.roll
 
     if args.max_dice:
-        f_kwargs['max_dice'] = args.max_dice
+        f_kwargs["max_dice"] = args.max_dice
 
     f_expr = " ".join(args.expression)
 
